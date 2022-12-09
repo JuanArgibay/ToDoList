8e349e13371fe6ac096908c11aeab223804766f8
ArgyDev <jmargibayg@gmail.com>
2022-12-09 15:33:44 +0100

    popUp create category done

--- src/components/Pop-up/Pop-up.jsx
+++ src/components/Pop-up/Pop-up.jsx
@@ -1,31 +1,60 @@
+import './pop-up.css'
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { AddIcon } from '../../ressources/AddIcon';
 
 export const PopUp = (args, task) => {
   const [modal, setModal] = useState(false);
+  const icons = ['🌍','👩‍👩‍👦','💪','🏘️','🔖','🚖','📺','📼'];
+  const [name, setName] = useState('');
+  const [color, setColor] = useState('transparent');
+  const [icon, setIcon] = useState(icons[0]);
+ 
 
   const toggle = () => setModal(!modal);
-  console.log(task);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    toggle();
+    console.log(icon);
+    console.log(name);
+    console.log(color);
+  }
 
   return (
     <div>
-        <label htmlFor='category'></label>
-       <li name='category' onClick={toggle}  className='category-task-container'>
-            <h3>titulo</h3>
-            <p>fecha</p>
-        </li>
+        <Button onClick={toggle}>
+          <AddIcon></AddIcon>
+        </Button>
       <Modal isOpen={modal} toggle={toggle} {...args}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalHeader toggle={toggle}>Add new category</ModalHeader>
         <ModalBody>
-          This is a text for the Pop-up
+          <form onSubmit={handleSubmit}>
+            <fieldset> 
+              <label htmlFor='name'>Whrite a name</label>
+              <input type='text' name='name' onChange={(e) => setName(e.target.value)}></input>
+            </fieldset>
+            <fieldset>
+              <label htmlFor='icon'>Select an icon</label>
+              <select name='icon' onChange={(e) => setIcon(e.target.value)}>
+              {icons.map((icon, index) => <option key={index}>{icon}</option>)}
+              </select>
+            </fieldset>
+            <fieldset> 
+              <label htmlFor='color'>Whrite the color</label>
+              <input type='text' name='color' placeholder= 'Example: E1C516' onChange={(e) => setColor(e.target.value)}></input>
+            </fieldset>
+              <Button color="primary">
+              Submit
+              </Button>{' '}
+              <Button color="secondary" onClick={() => toggle}>
+                Cancel
+              </Button>
+          </form>
+          
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Submit
-          </Button>{' '}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
+         
         </ModalFooter>
       </Modal>
     </div>
